dad9a9b004d7ec801e851cf6a5eaa89d4797cefd
GowthamNatsAlt <gowtham.nats@gmail.com>
2024-04-01 19:25:02 +0530

    Empty state done

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useOrganization, useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 import UploadButton from "./upload-button";
 import FileCard from "./file-card";
+import Image from "next/image";
 
 export default function Home() {
   // Logic to allow usage of both personal and organizational authorization
@@ -25,18 +26,39 @@ export default function Home() {
 
   return (
     <main className="container mx-auto pt-12">
-      <div className="flex justify-between items-center mb-8">
-        <h1 className="text-4xl font-bold">Your Files</h1>
-        {/* Dialog box for the file upload dialog box */}
-        <UploadButton />
-      </div>
-      <div className="grid grid-cols-4 gap-4">
-        {
-          files?.map(file => {
-            return <FileCard key={file._id} file={file} />
-          })
-        }
-      </div>
+
+      {files && files.length === 0 && (
+          <div className="flex flex-col gap-8 w-full items-center mt-24">
+            <Image 
+                alt="An image of a picture and directory icons"
+                width="300"
+                height="300"
+                src="/empty.svg"
+              />
+              <div className="text-2xl">
+                You have no files, upload one now.
+              </div>
+              <UploadButton />
+          </div>
+        )}
+
+      {files && files.length !== 0 && (
+        <>
+          <div className="flex justify-between items-center mb-8">
+            <h1 className="text-4xl font-bold">Your Files</h1>
+            {/* Dialog box for the file upload dialog box */}
+            <UploadButton />
+          </div>
+          <div className="grid grid-cols-4 gap-4">        
+            {
+              files?.map(file => {
+                return <FileCard key={file._id} file={file} />
+              })
+            }
+          </div>
+        </>
+      )}
+  
     </main>
   );
 }
